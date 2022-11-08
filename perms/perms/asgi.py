f73b5565fa520d7ba2a68d44528db1f25d21f329
LucaDulz <luca.dulz@med.uni-heidelberg.de>
2022-11-08 13:09:59 +0000

    project init

--- perms/perms/asgi.py
+++ perms/perms/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for permissions project.
+ASGI config for perms project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'permissions.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'perms.settings')
 
 application = get_asgi_application()
