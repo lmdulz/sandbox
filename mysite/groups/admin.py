a749d81d5f1f3f3b82aee265c229d33572312e2c
lmdulz2 <co251@uni-heidelberg.de>
2022-12-05 09:09:09 +0000

    hide save buttons Access admin

--- mysite/groups/admin.py
+++ mysite/groups/admin.py
@@ -5,5 +5,14 @@ class AccessAdmin(admin.ModelAdmin):
      list_display = ("name","access_type",
                      "group",
                      "node")
+
+     def has_add_permission(self, request, obj=None):
+        return False
+
+     def has_change_permission(self, request, obj=None):
+        return False
+
+     def has_delete_permission(self, request, obj=None):
+        return False
      
 admin.site.register(Access, AccessAdmin)
